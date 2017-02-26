9ba5880e1cc4645f91f0e0eb75fb0fe8810ba59f
KlavierCat <plin@tcd.ie>
2017-02-26 23:43:55 +0000

    Primitive Array in C

--- HelloObjectiveC/main.m
+++ HelloObjectiveC/main.m
@@ -58,6 +58,13 @@ int main(int argc, const char * argv[]) {
             default:
                 break;
         }
+        
+        // Primitive Arrays in C for performance intensive environment
+        int years[4] = {1968, 1970, 1989, 1999};
+        years[0] = 1967;
+        for (int i=0; i<4; i++) {
+            NSLog(@"The year at index %d is: %d", i, years[i]);
+        }
     }
     return 0;
 }
