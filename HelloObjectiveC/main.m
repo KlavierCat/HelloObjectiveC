90c3679a87e82a55d3147c5f322e6dbf22ecaed6
KlavierCat <plin@tcd.ie>
2017-02-26 19:32:29 +0000

    Initial Commit

---
+++ HelloObjectiveC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloObjectiveC
+//
+//  Created by klaviercat on 2/26/17.
+//  Copyright © 2017 klaviercat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
