f2b93161a104c758a1278fb1b2041af99366bbe9
KlavierCat <plin@tcd.ie>
2017-02-26 22:44:28 +0000

    typedef keyword to create new date type

--- HelloObjectiveC/main.m
+++ HelloObjectiveC/main.m
@@ -13,6 +13,9 @@
 #define PI 3.14159
 #define RAD_TO_DEG(radians) (radians * (180.0 / PI))
 
+// typedef keyword: create new data type or redefine existing ones
+typedef unsigned char ColorComponent;
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         double odometer = 9200.8;
@@ -27,6 +30,11 @@ int main(int argc, const char * argv[]) {
         
         double angle = PI / 2;
         NSLog(@"%f", RAD_TO_DEG(angle));
+        
+        ColorComponent red = 255;
+        ColorComponent green = 160;
+        ColorComponent blue = 0;
+        NSLog(@"Your paint job is (R: %hhu, G: %hhu, B: %hhu)", red, green, blue);
     }
     return 0;
 }
